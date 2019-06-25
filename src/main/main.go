45c3835a1343605e61b1b7c1af022c9b94401791
Nicolas <nikoremi97@hotmail.com>
2019-06-25 15:45:22 -0500

    Update README.md and some files with docs

--- src/main/main.go
+++ src/main/main.go
@@ -11,6 +11,7 @@ import (
 	"../handler"
 )
 
+// initRouter creates a new Chi Router and defines its routes and methods
 func initRouter() {
 
 	r := chi.NewRouter()
@@ -20,13 +21,13 @@ func initRouter() {
 
 	})
 
-	fmt.Printf("Starting server for testing HTTP...\n")
-
 	// Backend listen and serve in 192.168.1.57 port 3000
 	address := "192.168.1.57:3000"
 	if err := http.ListenAndServe(address, r); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Printf("Starting server for testing HTTP...\n")
 }
 
 // this is the main function of the backend. It creates the connection to Database, the Chi API router and defines its routes.
